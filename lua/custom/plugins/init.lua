7d6824fce343b578bc1f48286946a3d21a1ffa29
Curstantine <curstantine@proton.me>
2024-07-23 10:56:42 +0530

    feat: add js/ts lint and servers, remove colorscheme and fix formatting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- { -- You can easily change to a different colorscheme.
+	-- 	-- Change the name of the colorscheme plugin below, and then
+	-- 	-- change the command in the config to whatever the name of that colorscheme is.
+	-- 	--
+	-- 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	-- 	'sainnhe/sonokai',
+	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- 	init = function()
+	-- 		vim.g.sonokai_enable_italic = true
+	-- 		vim.g.sonokai_better_performance = 1
+	-- 		vim.g.sonokai_style = 'andromeda'
+	-- 		vim.cmd.colorscheme 'sonokai'
+	--
+	-- 		-- You can configure highlights by doing something like:
+	-- 		vim.cmd.hi 'Comment gui=none'
+	-- 	end,
+	-- },
+	{
+		'pmizio/typescript-tools.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+		opts = {},
+	},
+}
